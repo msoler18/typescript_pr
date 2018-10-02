5708e13271bed0c3d6206aeca94776d5b480ab84
msoler18 <msoler735@gmail.com>
2018-10-01 21:56:40 -0500

    Add string example

---
+++ strings.js
@@ -0,0 +1,5 @@
+var myName = "Miguel";
+var lastName = "Soler";
+var skill = 'Developer';
+console.log(myName);
+var greet = "Hello my name is " + myName + lastName + " my skill is:" + skill;
---
+++ strings.ts
@@ -0,0 +1,7 @@
+let myName:string = "Miguel"
+let lastName:string = "Soler"
+let skill:string = 'Developer'
+
+console.log(myName);
+
+let greet:string = `Hello my name is ${myName}${lastName} my skill is:${skill}`
