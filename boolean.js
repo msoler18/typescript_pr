f9f29e840b47fbc8c049166f33b7bc0cf14c015d
msoler18 <msoler735@gmail.com>
2018-10-01 21:37:06 -0500

    Add Example Boolean

---
+++ boolean.js
@@ -0,0 +1,12 @@
+var person = true;
+var otherPerson;
+if (person) {
+    console.log("Is a person");
+}
+else {
+    console.log("Not is person");
+}
+person = convertPerson();
+function convertPerson() {
+    return false;
+}
---
+++ boolean.ts
@@ -0,0 +1,14 @@
+let person:boolean = true;
+let otherPerson:boolean;
+
+  if (person){
+      console.log("Is a person");
+  } else{
+    console.log("Not is person");
+  }
+
+  person = convertPerson();
+
+  function convertPerson(){
+    return false;
+  }
