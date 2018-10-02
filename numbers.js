f618523a59e91c807b49834e8b8e78a45e35310c
msoler18 <msoler735@gmail.com>
2018-10-01 21:46:05 -0500

    Add number example

---
+++ numbers.js
@@ -0,0 +1,8 @@
+var number1 = 5;
+var number2 = 10;
+if (number1 > number2) {
+    console.log("Number 1 is larger");
+}
+else {
+    console.log("Number 2 is larger");
+}
---
+++ numbers.ts
@@ -0,0 +1,8 @@
+let number1:number = 5;
+let number2:number = 10;
+
+if(number1 > number2){
+  console.log("Number 1 is larger");
+} else{
+  console.log("Number 2 is larger");
+}
